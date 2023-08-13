7cac1d0c8f490221808b8d3ca98a7ccdc811f531
antique777 <1036652399@qq.com>
2023-08-13 20:00:07 +0800

    2

---
+++ nacos/src/main/java/org/javaboy/nacos/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.javaboy.nacos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${name}")
+    String name;
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return name;
+    }
+}
--- reilience4j-2/src/main/java/org/javaboy/reilience4j2/config/RestTemplateBean.java
+++ reilience4j-2/src/main/java/org/javaboy/reilience4j2/config/RestTemplateBean.java
@@ -21,4 +21,5 @@ public class RestTemplateBean {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate;
     }
+
 }
